51a6c08ad9f10505184cf0921c5b6e3d2412b230
4b-n4v <ab.navilon@gmail.com>
2024-08-05 15:25:03 -0500

    I guess we're using cmake instead of meson

--- README.md
+++
@@ -1,2 +0,0 @@
-# Practicing some FTXUI
-
---
+++ build/_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/flexbox_config.cpp.o.d
@@ -0,0 +1,3 @@
+_deps/ftxui-build/CMakeFiles/dom.dir/src/ftxui/dom/flexbox_config.cpp.o: \
+  /home/n4v/Projects/tui/build/_deps/ftxui-src/src/ftxui/dom/flexbox_config.cpp \
+  /home/n4v/Projects/tui/build/_deps/ftxui-src/include/ftxui/dom/flexbox_config.hpp
